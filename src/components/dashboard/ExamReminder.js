c2516de2bf7c26ea2fa61bb3104b88f4278157a2
nisarg <hammerbyte.nisarg@gmail.com>
2026-06-30 16:57:42 +0530

    tiny fix

--- src/components/dashboard/ExamReminder.js
+++ src/components/dashboard/ExamReminder.js
@@ -10,12 +10,13 @@ export default function ExamReminder({ className = "" }) {
     }
 
     return (
-        <div
-            className={`bg-red-500 text-white text-center p-2 fadein animation-duration-1000 animation-iteration-infinite ${className}`}
-        >
+        <div className={`bg-red-500 text-white text-center p-2 ${className}`}>
             <span className={`${TEXT_SMALL} font-semibold`}>
                 Your exam for {upcomingExam.subject_title} is scheduled for{" "}
-                {getReadableDate({ date: upcomingExam.exam_series_start_at })}.
+                <span className="font-bold underline fadein animation-duration-1000 animation-iteration-infinite">
+                    {getReadableDate({ date: upcomingExam.start_at })}
+                </span>
+                .
             </span>
         </div>
     );
